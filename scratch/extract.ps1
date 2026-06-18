8a5efa9ca80378af795d1c2b43667d0b09824203
bhumikaryautama17-droid <bhumikaryautama17@gmail.com>
2026-06-18 18:12:32 +0800

    Remove obsolete file scratch/extract.ps1

--- scratch/extract.ps1
+++
@@ -1,4 +0,0 @@
-$jsonText = Get-Content "scratch\generatePDF_raw.txt" -Raw
-$obj = ConvertFrom-Json "{ `"code`": $jsonText }"
-$obj.code | Out-File scratch\generatePDF_real.txt -Encoding UTF8
-Write-Host "Done!"
