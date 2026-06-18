20f0099d4156a10cd6c3d603ecc3fdaa04350922
bhumikaryautama17-droid <bhumikaryautama17@gmail.com>
2026-06-18 18:12:43 +0800

    Remove obsolete file scratch/find_lines.ps1

--- scratch/find_lines.ps1
+++
@@ -1,9 +0,0 @@
-$lines = Get-Content Code.js
-for ($i=0; $i -lt $lines.Length; $i++) {
-    if ($lines[$i] -match "^function generatePDF") {
-        Write-Host "Start: $i"
-    }
-    if ($lines[$i] -match "^// Terbilang helper") {
-        Write-Host "End: $i"
-    }
-}
