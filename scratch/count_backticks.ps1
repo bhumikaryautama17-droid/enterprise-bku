71f522f232168ee5ab4c5270d7f16611f20c7c15
bhumikaryautama17-droid <bhumikaryautama17@gmail.com>
2026-06-18 18:12:28 +0800

    Remove obsolete file scratch/count_backticks.ps1

--- scratch/count_backticks.ps1
+++
@@ -1,8 +0,0 @@
-$codeJs = Get-Content -Path "Code.js" -Raw
-$backticks = 0
-for ($i = 0; $i -lt $codeJs.Length; $i++) {
-    if ($codeJs[$i] -eq '`') {
-        $backticks++
-    }
-}
-Write-Host "Total backticks: $backticks"
